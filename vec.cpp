8f8f531bd574999e4941aae40870a7e2176803e3
Protoke <auffret.dev@gmail.com>
2017-12-04 16:04:00 +0100

    Correctifs vec et box

--- vec.cpp
+++ vec.cpp
@@ -12,20 +12,6 @@ Vec3::Vec3(double x, double y, double z) : x(x), y(y), z(z) { }
 Vec3::Vec3(const Vec2& v, double z) : x(v.x), y(v.y), z(z) { }
 Vec3::Vec3(const Vec3& v) : x(v.x), y(v.y), z(v.z) { }
 
-Point2::Point2() : Vec2() { }
-Point2::Point2(double x, double y) : Vec2(x, y) { }
-Point2::Point2(const Vec2& v) : Vec2(v) { }
-Point2::Point2(const Vec3& v) : Vec2(v) { }
-Point2::Point2(const Point2& p) : Vec2(p.x, p.y) { }
-Point2::Point2(const Point3& p) : Vec2(p.x, p.y) { }
-
-Point3::Point3() : Vec3() { }
-Point3::Point3(double x, double y, double z) : Vec3(x, y, z) { }
-Point3::Point3(const Vec2& v, double z) : Vec3(v, z) { }
-Point3::Point3(const Vec3& v) : Vec3(v) { }
-Point3::Point3(const Point2& p, double z) : Vec3(p.x, p.y, z) { }
-Point3::Point3(const Point3& p) : Vec3(p.x, p.y, p.z) { }
-
 double Vec2::length() {
 	return qSqrt(x * x + y * y);
 }
@@ -38,20 +24,8 @@ Vec2 operator+ (const Vec2& v1, const Vec2& v2) {
 	return Vec2(v1.x + v2.x, v1.y + v2.y);
 }
 
-Vec2 operator+ (const Point2& p, const Vec2& v) {
-	return Vec2(p.x + v.x, p.y + v.y);
-}
-
 Vec2 operator- (const Vec2& v1, const Vec2& v2) {
 	return Vec2(v1.x - v2.x, v1.y - v2.y);
-}
-
-Vec2 operator- (const Point2& p1, const Point2& p2) {
-	return Vec2(p1.x - p2.x, p1.y - p2.y);
-}
-
-Vec2 operator- (const Point2& p, const Vec2& v) {
-	return Vec2(p.x - v.x, p.y - v.y);
 }
 
 Vec2 operator* (const Vec2& v, double d) {
@@ -66,20 +40,8 @@ Vec3 operator+ (const Vec3& v1, const Vec3& v2) {
 	return Vec3(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
 }
 
-Vec3 operator+ (const Point3& p, const Vec3& v) {
-	return Vec3(p.x + v.x, p.y + v.y, p.z + v.z);
-}
-
 Vec3 operator- (const Vec3& v1, const Vec3& v2) {
 	return Vec3(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
-}
-
-Vec3 operator- (const Point3& p1, const Point3& p2) {
-	return Vec3(p1.x - p2.x, p1.y - p2.y, p1.z - p2.z);
-}
-
-Vec3 operator- (const Point3& p, const Vec3& v) {
-	return Vec3(p.x - v.x, p.y - v.y, p.z - v.z);
 }
 
 Vec3 operator* (const Vec3& v, double d) {
@@ -97,15 +59,6 @@ bool operator== (const Vec2& v1, const Vec2& v2){
 bool operator== (const Vec3& v1, const Vec3& v2){
     return v1.x == v2.x && v1.y == v2.y && v1.z == v2.z;
 }
-
-bool operator== (const Point2& p1, const Point2& p2){
-    return p1.x == p2.x && p1.y == p2.y;
-}
-
-bool operator== (const Point3& p1, const Point3& p2){
-    return p1.x == p2.x && p1.y == p2.y && p1.z == p2.z;
-}
-
 
 double dot(const Vec3& va, const Vec3& vb) {
 	return va.x * vb.x + va.y * vb.y + va.z * vb.z;
--- vec.h
+++ vec.h
@@ -7,8 +7,6 @@ public:
 
 class Vec2;
 class Vec3;
-class Point2;
-class Point3;
 
 class Vec2 : public Vec {
 public:
@@ -34,46 +32,18 @@ public:
 	double x, y, z;
 };
 
-class Point2 : public Vec2 {
-public:
-	Point2();
-	Point2(double x, double y);
-	Point2(const Vec2& v);
-	Point2(const Vec3& v);
-	Point2(const Point2& p);
-	Point2(const Point3& p);
-};
-
-class Point3 : public Vec3 {
-public:
-	Point3();
-	Point3(double x, double y, double z);
-	Point3(const Vec2& v,  double z);
-	Point3(const Vec3& v);
-	Point3(const Point2& p, double z);
-	Point3(const Point3& p);
-};
-
 Vec2 operator+ (const Vec2& v1, const Vec2& v2);
-Vec2 operator+ (const Point2& p, const Vec2& v);
 Vec2 operator- (const Vec2& v1, const Vec2& v2);
-Vec2 operator- (const Point2& p1, const Point2& p2);
-Vec2 operator- (const Point2& p, const Vec2& v);
 Vec2 operator* (const Vec2& v, double d);
 Vec2 operator/ (const Vec2& v, double d);
 
 Vec3 operator+ (const Vec3& v1, const Vec3& v2);
-Vec3 operator+ (const Point3& p, const Vec3& v);
 Vec3 operator- (const Vec3& v1, const Vec3& v2);
-Vec3 operator- (const Point3& p1, const Point3& p2);
-Vec3 operator- (const Point3& p, const Vec3& v);
 Vec3 operator* (const Vec3& v, double d);
 Vec3 operator/ (const Vec3& v, double d);
 
 bool operator== (const Vec2& v1, const Vec2& v2);
 bool operator== (const Vec3& v1, const Vec3& v2);
-bool operator== (const Point2& p1, const Point2& p2);
-bool operator== (const Point3& p1, const Point3& p2);
 
 double dot(const Vec3& va, const Vec3& vb);
 Vec3 cross(const Vec3& va, const Vec3& vb);
