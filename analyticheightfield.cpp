e528b947cad94ab364ae4211ff784a1b09625d75
Protoke <auffret.dev@gmail.com>
2017-12-04 17:54:18 +0100

    AnalyticHeightField

---
+++ analyticheightfield.cpp
@@ -0,0 +1,26 @@
+#include "analyticheightfield.h"
+
+AnalyticHeightField::AnalyticHeightField() { }
+
+AnalyticHeightField::AnalyticHeightField(double (*noiseFunction)(double,double),
+                                         double A, double T,
+                                         QVector<double> offsets, int levels) :
+    m_A(A), m_T(T), m_levels(levels)
+{
+    m_noiseFunction = noiseFunction;
+    m_offsets = offsets;
+}
+
+double AnalyticHeightField::height(const Vec2 &v){
+    double height = 0;
+
+    double pow2 = 1;
+
+    for(int i = 0; i < m_levels; ++i){
+        height += m_noiseFunction(v.x/(m_T/pow2) + m_offsets[i], v.y/(m_T/pow2) + m_offsets[i]) * (m_A/pow2);
+        pow2 *= 2;
+    }
+
+    return height;
+}
+
---
+++ analyticheightfield.h
@@ -0,0 +1,29 @@
+#ifndef ANALYTICHEIGHTFIELD_H
+#define ANALYTICHEIGHTFIELD_H
+
+#include <QVector>
+
+#include "vec.h"
+#include "qmath.h"
+
+inline double noiseSin(double x, double y){
+    return 1 - (qSin(x / 6.28) + 1.0) / 2.0 + (sin(y / 6.28) + 1.0) / 2.0;
+}
+
+class AnalyticHeightField
+{
+public:
+    AnalyticHeightField();
+    AnalyticHeightField(double (*noiseFunction)(double,double), double A, double T,
+                        QVector<double> offsets, int levels);
+
+    double height(const Vec2& v);
+
+private:
+    double (*m_noiseFunction)(double,double);
+    double m_A, m_T;
+    QVector<double> m_offsets;
+    int m_levels;
+};
+
+#endif // ANALYTICHEIGHTFIELD_H
